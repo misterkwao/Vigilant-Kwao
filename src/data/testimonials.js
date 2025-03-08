ad5cd413f85c7971eccdd789e27ebefd3e607966
misterkwao <vigilantkwao@gmail.com>
2025-03-08 16:43:56 +0000

    testimonial update

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -7,12 +7,12 @@ export const testimonialsData = [
         "name": "Aaron Otu",
         "description": "I’ve had the privilege of journeying through university with Vigilant Kwao, from level of KNUST 100 to 400. He’s been the glue holding our group together, always fostering unity and keeping our bond strong. Beyond his remarkable interpersonal skills, he’s an exceptional programmer—highly skilled in various frameworks and always ready to share his knowledge. It’s rare to find someone who excels both technically and socially, but Obobisa does so effortlessly. Proud to have walked this path alongside him.",
     },
-    {
-        "name": "Nadine Yankey",
-        "description": "",
-    },
+    // {
+    //     "name": "Nadine Yankey",
+    //     "description": "",
+    // },
     {
         "name": "Francis Zigah",
-        "description": "",
+        "description": "Worked with him on a few projects and he's an exceptional team player with leadership skills. Determined and has a drive to achieve his goals. A great guy with big things ahead.",
     },
 ]
