55dfbc41118ba868f2a9578359cb24068223ed4a
misterkwao <vigilantkwao@gmail.com>
2025-04-28 22:38:54 +0000

    testimonial update

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -5,14 +5,14 @@ export const testimonialsData = [
     },
     {
         "name": "Aaron Otu",
-        "description": "I’ve had the privilege of journeying through university with Vigilant Kwao, from level of KNUST 100 to 400. He’s been the glue holding our group together, always fostering unity and keeping our bond strong. Beyond his remarkable interpersonal skills, he’s an exceptional programmer—highly skilled in various frameworks and always ready to share his knowledge. It’s rare to find someone who excels both technically and socially, but Obobisa does so effortlessly. Proud to have walked this path alongside him.",
+        "description": "I’ve had the privilege of journeying through university with Vigilant Kwao, from level 100 of KNUST to 400. He’s been the glue holding our group together, always fostering unity and keeping our bond strong. Beyond his remarkable interpersonal skills, he’s an exceptional programmer—highly skilled in various frameworks and always ready to share his knowledge. It’s rare to find someone who excels both technically and socially, but Obobisa does so effortlessly. Proud to have walked this path alongside him.",
     },
-    // {
-    //     "name": "Nadine Yankey",
-    //     "description": "",
-    // },
     {
         "name": "Francis Zigah",
         "description": "Worked with him on a few projects and he's an exceptional team player with leadership skills. Determined and has a drive to achieve his goals. A great guy with big things ahead.",
     },
+    {
+        "name": "Nadine Yankey",
+        "description": "I had the honor of studying with Vigilant Kwao Obobisa during university, and he remains one of the brightest minds I’ve ever met. Vigilant has exceptional programming skills across multiple languages and an incredible ability to calmly and precisely solve complex problems. He was always generous with his time, offering real support whenever I or others needed help. His guidance made a real difference during some of the toughest moments in my academic journey. I have no doubt that any organization would be lucky to have someone with Vigilant’s expertise, reliability, and genuine spirit.",
+    }
 ]
